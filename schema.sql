44bb41f481fc0eb711071d36708e40d832702428
namratawaybhase <namrata.waybhase@gmail.com>
2021-02-24 21:09:44 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,30 +1,30 @@
 DROP TABLE IF EXISTS User;
-DROP TABLE IF EXISTS post;
+DROP TABLE IF EXISTS Vendor;
 
 CREATE TABLE User (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   username TEXT UNIQUE NOT NULL,
   password TEXT NOT NULL,
   firstname TEXT NOT NULL,
-  lastname TEXT NOT NULL
+  lastname TEXT NOT NULL,
   email TEXT NOT NULL
 );
 
 CREATE TABLE Vendor (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   name TEXT NOT NULL,
   address TEXT NOT NULL,
-  phone_no TEXT NOT NULL,
+  phone_no TEXT NOT NULL
 );
+
 Insert into Vendor
 values(110,'Chipotle','Redwood City','713-792-9390');
 
 Insert into Vendor
-values('McDonald','Mountain View','713-668-5882');
+values(111,'McDonald','Mountain View','713-668-5882');
 
 Insert into Vendor
-values('Panda Express','Mountain View','713-668-7898');
+values(222,'Panda Express','Mountain View','713-668-7898');
 
 Insert into Vendor
-values('Burger King','San Jose','713-989-1111');
+values(333,'Burger King','San Jose','713-989-1111');
