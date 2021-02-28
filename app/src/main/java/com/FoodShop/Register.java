aed9c4b8cd4fc7de81c7e3068d7363d301e636e4
Mikaela M <59987624+MikaelaMontaos@users.noreply.github.com>
2021-02-28 12:59:50 -0800

    Update Register.java
    Integrate to Flask server

--- app/src/main/java/com/FoodShop/Register.java
+++ app/src/main/java/com/FoodShop/Register.java
@@ -21,45 +21,38 @@ import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
 
-public class Register extends AppCompatActivity {
-    CircularProgressButton btnRegister;
-    
-    // text field id
-    EditText email, firstName, lastName, password;
+public class LoginActivity extends AppCompatActivity {
+    CircularProgressButton btnlogin;
+
+    // text field
+    EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
-        btnRegister = findViewById(R.id.registerBtnReg);
+        setContentView(R.layout.activity_login);
+        btnlogin = findViewById(R.id.loginbtnlogin);
 
         // text field id
-        email = findViewById(R.id.et_email);
-        firstName = findViewById(R.id.et_firstname);
-        lastName = findViewById(R.id.et_lastname);
-        password = findViewById(R.id.et_password);
+        email = findViewById(R.id.et_email_login);
+        password = findViewById(R.id.et_password_login);
 
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+        btnlogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 // get user input
                 String emailS = email.getText().toString();
-                String firstNameS = firstName.getText().toString();
-                String lastNameS = lastName.getText().toString();
                 String passwordS = password.getText().toString();
-                // if not empty
-                if(emailS != null && firstNameS != null && lastNameS != null && passwordS != null){
+                if(emailS != null && passwordS != null) {
                     // connect to server
                     OkHttpClient okHttpClient = new OkHttpClient();
                     // post
                     RequestBody formBody = new FormBody.Builder()
                             .add("email", emailS)
-                            .add("firstname", firstNameS)
-                            .add("lastname", lastNameS)
                             .add("password", passwordS)
                             .build();
-                    Request request = new Request.Builder().url("http://10.0.2.2:5000/Users").post(formBody).build();
+                    Request request = new Request.Builder().url("http://10.0.2.2:5000/login").post(formBody).build();
                     // async call to connect
                     okHttpClient.newCall(request).enqueue(new Callback() {
                         @Override
@@ -67,26 +60,25 @@ public class Register extends AppCompatActivity {
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    Toast.makeText(Register.this, "Cannot connect to server. Please try again.", Toast.LENGTH_LONG).show();
+                                    Toast.makeText(LoginActivity.this, "Cannot connect to server. Please try again.", Toast.LENGTH_LONG).show();
                                 }
                             });
                         }
                         @Override
                         public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                            Toast.makeText(Register.this, "Registration successful", Toast.LENGTH_LONG).show();
+                            Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_LONG).show();
                         }
                     });
-                    Toast.makeText(Register.this, "Registration successful", Toast.LENGTH_LONG).show();
+                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
                     // todo: MainActivity goes back to homepage
 
-                    Intent intent = new Intent(Register.this, MainActivity.class);
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
 
                 }
                 else
-                    Toast.makeText(Register.this, "Please complete form", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, "Please complete form", Toast.LENGTH_SHORT).show();
             }
         });
     }
 }
-
