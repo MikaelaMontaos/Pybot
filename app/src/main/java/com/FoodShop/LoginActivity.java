50a2e215f0522152e644d8aa1909fbb26d87ba34
Mikaela M <59987624+MikaelaMontaos@users.noreply.github.com>
2021-03-16 11:26:47 -0700

    Update LoginActivity.java
    Finalized version of login and sign up integration

--- app/src/main/java/com/FoodShop/LoginActivity.java
+++ app/src/main/java/com/FoodShop/LoginActivity.java
@@ -69,8 +69,6 @@ public class LoginActivity extends AppCompatActivity {
                             Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_LONG).show();
                         }
                     });
-                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
-                    // todo: MainActivity goes back to homepage
 
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
