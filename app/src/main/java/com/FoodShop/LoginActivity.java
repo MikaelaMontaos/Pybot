de1f6bd36b7fe3c566a19b8b60a025e8397737fa
Mikaela M <59987624+MikaelaMontaos@users.noreply.github.com>
2021-04-05 18:26:07 -0700

    Update LoginActivity.java
    Debug

--- app/src/main/java/com/FoodShop/LoginActivity.java
+++ app/src/main/java/com/FoodShop/LoginActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import br.com.simplepass.loadingbutton.customViews.CircularProgressButton;
 import okhttp3.Call;
@@ -20,6 +21,7 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class LoginActivity extends AppCompatActivity {
     CircularProgressButton btnlogin;
@@ -44,15 +46,20 @@ public class LoginActivity extends AppCompatActivity {
                 // get user input
                 String emailS = email.getText().toString();
                 String passwordS = password.getText().toString();
-                if(emailS != null && passwordS != null) {
+
+                // if form is not empty
+                if (!emailS.isEmpty() && !passwordS.isEmpty()) {
+
                     // connect to server
                     OkHttpClient okHttpClient = new OkHttpClient();
+
                     // post
                     RequestBody formBody = new FormBody.Builder()
                             .add("email", emailS)
                             .add("password", passwordS)
                             .build();
                     Request request = new Request.Builder().url("http://10.0.2.2:5000/login").post(formBody).build();
+
                     // async call to connect
                     okHttpClient.newCall(request).enqueue(new Callback() {
                         @Override
@@ -60,21 +67,45 @@ public class LoginActivity extends AppCompatActivity {
                             runOnUiThread(new Runnable() {
                                 @Override
                                 public void run() {
-                                    Toast.makeText(LoginActivity.this, "Cannot connect to server. Please try again.", Toast.LENGTH_LONG).show();
+                                    Toast.makeText(LoginActivity.this, "Cannot connect to server. Please try again.", Toast.LENGTH_SHORT).show();
                                 }
                             });
                         }
+
                         @Override
-                        public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
-                            Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_LONG).show();
+                        public void onResponse(@NotNull Call call, @NotNull final Response response) throws IOException {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (response.code() != 404 && response.code() != 403) {
+                                        Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+
+                                        // extract response to get user id (UserId.java)
+                                        String res = null;
+                                        try {
+                                            res = Objects.requireNonNull(Objects.requireNonNull(response.body()).string());
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                        UserId userId = new UserId(res);
+                                        int user_id = userId.getId();
+                                        //Toast.makeText(LoginActivity.this, "user id " + user_id, Toast.LENGTH_SHORT).show();
+
+                                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+                                        // send user id to MainActivity
+                                        Bundle bundle = new Bundle();
+                                        bundle.putInt("userId", user_id);
+                                        intent.putExtras(bundle);
+
+                                        startActivity(intent);
+                                    }
+                                    else Toast.makeText(LoginActivity.this, "Incorrect email or password", Toast.LENGTH_SHORT).show();
+                                }
+                            });
                         }
                     });
-
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-
-                }
-                else
+                } else
                     Toast.makeText(LoginActivity.this, "Please complete form", Toast.LENGTH_SHORT).show();
             }
         });
