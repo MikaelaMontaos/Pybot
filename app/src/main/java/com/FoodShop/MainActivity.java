dae8e797d553de3e01ce35284c41cc545dfa05a5
Mikaela M <59987624+MikaelaMontaos@users.noreply.github.com>
2021-04-05 18:27:41 -0700

    Update MainActivity.java
    Back button improvement

--- app/src/main/java/com/FoodShop/MainActivity.java
+++ app/src/main/java/com/FoodShop/MainActivity.java
@@ -2,10 +2,10 @@ package com.FoodShop;
 
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
@@ -17,24 +17,36 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.transition.Slide;
 
+import com.fragments.ResMenuFragment;
+import com.fragments.RestaurantFragment;
 import com.google.android.material.navigation.NavigationView;
-
 
 public class MainActivity extends AppCompatActivity {
     NavigationView navview;
     DrawerLayout drawerLayout;
     FragmentTransaction fragmentTransaction;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme_NoActionBar);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-         Restaurant restaurant = new Restaurant();
+
+        RestaurantFragment restaurantFragment = new RestaurantFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.mainFrame, restaurant);
+        fragmentTransaction.add(R.id.mainFrame, restaurantFragment);
+
+        // get user id
+        Bundle uExtras = this.getIntent().getExtras();
+        int userId = uExtras.getInt("userId");
+        //Toast.makeText(MainActivity.this, "user id " + userId, Toast.LENGTH_SHORT).show();
+
+        // send user id to RestaurantFragment
+        Bundle bundle = new Bundle();
+        bundle.putInt("userId", userId);
+        restaurantFragment.setArguments(uExtras);
+
         fragmentTransaction.commit();
 
         navview = findViewById(R.id.navview);
@@ -44,8 +56,6 @@ public class MainActivity extends AppCompatActivity {
         navview.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-
                 drawerLayout.closeDrawers();
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -55,37 +65,31 @@ public class MainActivity extends AppCompatActivity {
                     navview.getMenu().getItem(i).setChecked(false);
                 }
                 switch (item.getItemId()) {
-
                     case R.id.nav_home:
                         item.setChecked(true);
-
-                        Fragment fragment = new Restaurant();
+                        Fragment fragment = new RestaurantFragment();
                         fragment.setEnterTransition(new Slide(Gravity.BOTTOM));
                         fragment.setExitTransition(new Slide(Gravity.TOP));
                         fragmentTransaction.replace(R.id.mainFrame, fragment);
                         fragmentTransaction.commit();
-
                         break;
                     case R.id.nav_order:
-
                         item.setChecked(true);
-                        fragmentTransaction.replace(R.id.mainFrame, new Restaurant());
+                        fragmentTransaction.replace(R.id.mainFrame, new RestaurantFragment());
                         fragmentTransaction.commit();
                         break;
                     case R.id.nav_cart:
-
                         item.setChecked(true);
-                        fragmentTransaction.replace(R.id.mainFrame, new ResMenu());
+                        fragmentTransaction.replace(R.id.mainFrame, new ResMenuFragment());
                         fragmentTransaction.commit();
                         break;
                     case R.id.nav_history:
-                        Intent intent = new Intent(MainActivity.this, Cart.class);
-
+                        Intent intent = new Intent(MainActivity.this, CartActivity.class);
                         startActivity(intent);
                         break;
                     case R.id.nav_my_account:
                         item.setChecked(true);
-                        fragmentTransaction.replace(R.id.mainFrame, new Restaurant());
+                        fragmentTransaction.replace(R.id.mainFrame, new RestaurantFragment());
                         fragmentTransaction.commit();
                         break;
                     default:
@@ -94,8 +98,6 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-
     }
     @Override
     public void onBackPressed() {
@@ -120,5 +122,4 @@ public class MainActivity extends AppCompatActivity {
             dialog.show();
         }
     }
-
 }
