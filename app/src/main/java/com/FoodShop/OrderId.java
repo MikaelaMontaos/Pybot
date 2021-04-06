d0b0ef172ba66257af3646957c5b68c069fe9746
Mikaela M <59987624+MikaelaMontaos@users.noreply.github.com>
2021-04-05 18:28:09 -0700

    Add files via upload

---
+++ app/src/main/java/com/FoodShop/OrderId.java
@@ -0,0 +1,23 @@
+package com.FoodShop;
+
+public class OrderId {
+
+    private int id;
+    public OrderId(int id) {
+        this.id = id;
+    }
+
+//    public String getOrder_text() {
+//        return order_text;
+//    }
+//
+//    public void setOrder_text(String order_text) {
+//        this.order_text = order_text;
+//    }
+
+    public int getId() { return id;}
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
